6b898a943b76dde7968e3d551d9bce106e50bd31
zilog <zilog@zilogs-macbook-pro.local>
2011-01-01 04:33:34 +0100

    added example

---
+++ example/testApp.cpp
@@ -0,0 +1,98 @@
+#include "testApp.h"
+
+
+
+//using namespace veryHandy;
+
+
+//--------------------------------------------------------------
+void testApp::setup(){	
+	p1.load("ikStaar_palette_10x10-2.png", 4);
+	p2.load("ikStaar_palette_10x10.png", 10);
+}
+
+
+//--------------------------------------------------------------
+void testApp::update(){
+	
+	ofBackground(100,100,100);
+	
+}
+
+void testApp::drawPalette(ofxGraphicPalette p, int x, int y) {
+	ofPushMatrix();
+		for(int i = 0; i < p.getColorCount(); i++) {
+			ofColor col = p.getColor(i);
+			ofSetColor(col.r, col.g, col.b, 255);
+			ofFill();
+			ofRect(x, y, 10, 10);
+			ofTranslate(10, 0, 0);
+		}
+	ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetColor(0xffffff);
+
+	string tstamp = getTimestamp();
+	unsigned long epoch = getEpoch( tstamp );
+	
+	stringstream s;
+	s << "tstamp " << tstamp << endl << "epoch " << epoch;
+	ofDrawBitmapString( s.str(), 20, 20);	
+
+	stringstream s2;
+	s2 << "palette #1 no. colors " << p1.getColorCount() << endl << "pallete #2 no. colors: " << p2.getColorCount();
+	ofDrawBitmapString( s2.str(), 20, 50);	
+	
+	drawPalette(p1, 10, 80);
+	drawPalette(p2, 10, 100);
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){ 
+	
+	// in fullscreen mode, on a pc at least, the 
+	// first time video settings the come up
+	// they come up *under* the fullscreen window
+	// use alt-tab to navigate to the settings
+	// window. we are working on a fix for this...
+	
+	if (key == 's' || key == 'S'){
+	}
+	
+	
+}
+
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){ 
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+	
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
